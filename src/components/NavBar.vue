79448d2bc8d2c2ea66ad6d4700705043b4616ecf
Camaechi <amaechi@yellowcard.io>
2022-07-08 01:13:59 +0400

    added donate link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,18 @@
   <div id="nav" class="nav">
     <router-link to="/" class="brand">AceTech+1LCards</router-link>
     <nav>
-      <router-link :to="{ name: 'user-create' }">Create User</router-link>
+      <router-link :to="{ name: 'user-create' }">Create User</router-link> |
+      <a
+        href="https://github.com/acetechnology/acetech-oneliquidity-virtual-cards-sandbox-demo/watchers"
+        target="_blank"
+        >Download</a
+      >
+      |
+      <a
+        href="https://www.paypal.com/donate/?hosted_button_id=5QULG9MZSLL5J"
+        target="_blank"
+        >Donate</a
+      >
     </nav>
   </div>
 </template>
@@ -21,7 +32,7 @@ export default {};
 .nav > .brand {
   font-family: "Montserrat", sans-serif;
   font-weight: 700;
-  font-size: 1.5em;
+  font-size: 1.2em;
   color: #7b61ff;
   text-decoration: none;
 }
